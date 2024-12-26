96f68acf8797a421981deb159f612412aa52f6f2
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2024-12-26 13:15:38 +0100

    🎨Add: Scroll button + fix timeline

--- src/components/TimelineEducation.vue
+++ src/components/TimelineEducation.vue
@@ -2,77 +2,168 @@
 </script>
 
 <template>
-  <div class="timeline">
-    <header class="timeline-header">
-      <span class="tag is-medium is-primary">2025</span>
-    </header>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">2024 - 2025</p>
-        <p>BUT Informatique 3e année - Parcours RACDV</p>
+  <div id="timeline-container">
+    <div class="timeline">
+      <div class="outer">
+        <div class="card">
+          <div class="info">
+            <h3 class="title">Title 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3 class="title">Title 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3 class="title">Title 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3 class="title">Title 4</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="info">
+            <h3 class="title">Title 5</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
       </div>
-    </div>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">Juillet 2024 - Aout 2025</p>
-        <p>Alternance développement web</p>
-      </div>
-    </div>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">Avril 2024 - Juin 2024</p>
-        <p>Stage développement web</p>
-      </div>
-    </div>
-
-
-    <header class="timeline-header">
-      <span class="tag is-medium is-primary">2024</span>
-    </header>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">2023 - 2024</p>
-        <p>BUT Informatique 2e année - Parcours RACDV</p>
-      </div>
-    </div>
-
-
-    <header class="timeline-header">
-      <span class="tag is-medium is-primary">2023</span>
-    </header>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">2022 - 2023</p>
-        <p>BUT Informatique 1e année</p>
-      </div>
-    </div>
-
-
-    <header class="timeline-header">
-      <span class="tag is-medium is-primary">2022</span>
-    </header>
-
-    <div class="timeline-item">
-      <div class="timeline-marker"></div>
-      <div class="timeline-content">
-        <p class="heading">2019 - 2022</p>
-        <p>Baccalauréat Général (Spécialités NSI/AMC)</p>
-      </div>
-    </div>
-
-
-    <div class="timeline-header">
-      <span class="tag is-medium is-primary">2019</span>
     </div>
   </div>
+
+  <!--  <div class="">-->
+<!--    <header class="timeline-header">-->
+<!--      <span class="tag is-medium is-primary">2025</span>-->
+<!--    </header>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">2024 - 2025</p>-->
+<!--        <p>BUT Informatique 3e année - Parcours RACDV</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">Juillet 2024 - Aout 2025</p>-->
+<!--        <p>Alternance développement web</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">Avril 2024 - Juin 2024</p>-->
+<!--        <p>Stage développement web</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+<!--    <header class="timeline-header">-->
+<!--      <span class="tag is-medium is-primary">2024</span>-->
+<!--    </header>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">2023 - 2024</p>-->
+<!--        <p>BUT Informatique 2e année - Parcours RACDV</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+<!--    <header class="timeline-header">-->
+<!--      <span class="tag is-medium is-primary">2023</span>-->
+<!--    </header>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">2022 - 2023</p>-->
+<!--        <p>BUT Informatique 1e année</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+<!--    <header class="timeline-header">-->
+<!--      <span class="tag is-medium is-primary">2022</span>-->
+<!--    </header>-->
+
+<!--    <div class="timeline-item">-->
+<!--      <div class="timeline-marker"></div>-->
+<!--      <div class="timeline-content">-->
+<!--        <p class="heading">2019 - 2022</p>-->
+<!--        <p>Baccalauréat Général (Spécialités NSI/AMC)</p>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+<!--    <div class="timeline-header">-->
+<!--      <span class="tag is-medium is-primary">2019</span>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
+
+<style scoped>
+#timeline-container {
+  display: flex;
+  justify-content: center;
+}
+
+/* Timeline Container */
+.timeline {
+  margin: 20px auto;
+  padding: 20px;
+}
+
+/* Outer Layer with the timeline border */
+.outer {
+  border-left: 2px solid #333;
+}
+
+/* Card container */
+.card {
+  position: relative;
+  margin: 0 0 20px 20px;
+  padding: 10px;
+  background: #003830;
+  color: #05f3d0;
+  border-radius: 8px;
+  max-width: 400px;
+}
+
+/* Information about the timeline */
+.info {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+/* Title of the card */
+.title {
+  color: white;
+  position: relative;
+}
+
+/* Timeline dot  */
+.title::before {
+  content: "";
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background: white;
+  border-radius: 999px;
+  left: -35px;
+  border: 3px solid #003830;
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
 import TimelineEducation from '@/components/TimelineEducation.vue'
+
+function scrollToSection(section: string) {
+  const nextSection = document.getElementById(section);
+  if (nextSection) {
+    nextSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
 </script>
 
 <template>
   <section class="hero is-fullheight fullwidth">
     <div class="hero-body">
-      <div class="">
+      <div>
         <p class="title">Portfolio</p>
         <p class="subtitle">Bienvenue sur mon portfolio</p>
+      </div>
+
+      <div class="jump-button-container">
+        <button class="jump-button" @click="scrollToSection('timeline-edu')">
+          ↓
+        </button>
       </div>
     </div>
   </section>
 
-  <div class="fullwidth">
+  <div id="timeline-edu" class="fullwidth pt-6">
     <TimelineEducation />
   </div>
 </template>
@@ -21,5 +34,29 @@ import TimelineEducation from '@/components/TimelineEducation.vue'
 .fullwidth {
   width: 100%;
   min-width: 10vw;
+}
+
+.jump-button-container {
+  position: absolute;
+  bottom: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.jump-button {
+  background-color: #3273dc;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: transform 0.3s ease, background-color 0.3s ease;
+}
+
+.jump-button:hover {
+  background-color: #275aa8;
+  transform: scale(1.1) rotate(15deg);
 }
 </style>
